7a177cac2a230f60a5937340ed7a33e62f6dfa53
Stevtm <steventmaddison@outlook.com>
2021-06-26 20:30:35 -0400

    create post and put requests for User model

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -39,4 +39,34 @@ const userController = {
 				res.status(400).json(err);
 			});
 	},
+
+	// create user
+	createUser({ body }, res) {
+		User.create(body)
+			.then((dbUserData) => {
+				res.status(200).json(dbUserData);
+			})
+			.catch((err) => {
+				res.status(400).json(err);
+			});
+	},
+
+	// update user by id
+	updateUser({ params, body }, res) {
+		User.findOneAndUpdate({ _id: params.id }, body, {
+			new: true,
+			runValidators: true,
+		})
+			.then((dbUserData) => {
+				if (!dbUserData) {
+					res.status(404).json({ message: "No user found with this id" });
+					return;
+				}
+				res.status(200).json(dbUserData);
+			})
+			.catch((err) => {
+				console.log(err);
+				res.status(400).json(err);
+			});
+	},
 };
